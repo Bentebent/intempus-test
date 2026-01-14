a99bcd9b82307b98876b7cb09ad256826462ab86
unknown <henriksson.tim@gmail.com>
2026-01-14 15:23:27 +0100

    feat: add intempus api uri config

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Config(BaseSettings):
     model_config = SettingsConfigDict(validate_default=False)
+    intempus_api_uri: str
     intempus_api_user: str
     intempus_api_key: str
     db_name: str
